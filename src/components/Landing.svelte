e08a1775b55bcfb57698291ab515fa7bc60eb3f8
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-08-04 19:55:20 -0700

    work on bottom boxes

--- src/components/Landing.svelte
+++ src/components/Landing.svelte
@@ -6,6 +6,7 @@
   import side_leaves from '$lib/Assets/Landing/side_leaves.png';
   import light_waves from '$lib/Assets/Landing/light_waves.png';
   import dark_waves from '$lib/Assets/Landing/dark_waves.png';
+	import lilly_pad from '$lib/Assets/Landing/lilly_pad.png';
 
 </script>
 
@@ -23,23 +24,42 @@
 			<span class="sm:text-sm md:text-lg lg:text-5xl z-10 font-extrabold">
 					FullyBeyond: Across the Pond
 			</span>
-			<span class="sm:text-sm md:text-lg lg:text-3xl z-10 font-black mt-4">
+			<span class="sm:text-sm md:text-lg lg:text-4xl z-10 font-black mt-4">
 					CSUF's First Designathon
 			</span>
-			<span class="sm:text-sm md:text-lg lg:text-3xl z-10 font-black mt-4">
+			<span class="sm:text-sm md:text-lg lg:text-4xl z-10 font-black mt-4">
 					November 9th - 10th
 			</span>
+		</div>
+
+		<img src={hills} alt="Hills" class="w-full block ml-auto mr-auto left-0 absolute z-10 top-28"/>
+		<img src={grass} alt="Grass" class="w-full block ml-auto mr-auto bottom-[-155px] left-0 absolute z-10" />
+		<img src={side_leaves} alt="Side Leaves" class="block w-2/5 ml-auto mr-auto top-0 right-0 absolute z-30" />
+		
+		<img src={dark_waves} alt="Dark Waves" class="w-full block absolute left-0 z-10 bottom-0" />
+		<img src={light_waves} alt="Light Waves" class="w-full block absolute left-0 z-10 bottom-0" />
+		<div class="absolute	bg-[#78BFCE] left-0 w-full h-full z-10">
+			<div class="absolute bg-[#78BFCE] left-0 w-full  h-full z-0">
+				<div class="flex justify-center space-x-20 mt-20 relative">
+					<div class="border-dashed border-8 border-[#89CAD8] w-80 h-52 z-20 relative">
+						<span class="text-[#000000] text-3xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">NOV. 9 - NOV.10</span>
+						<img src={lilly_pad} alt="Lilly Pad" class="absolute w-40 bottom-2 z-10" />
+					</div>
+					<div class="border-dashed border-8 border-[#89CAD8] w-80 h-52 z-20 relative">
+						<span class="text-[#000000] text-3xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">24 HOURS</span>
+						<img src={lilly_pad} alt="Lilly Pad" class="absolute w-40 bottom-2 z-10" />
+					</div>
+					<div class="border-dashed border-8 border-[#89CAD8] w-80 h-52 z-20 relative">
+						<span class="text-[#000000] text-3xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">VIRTUAL</span>
+						<img src={lilly_pad} alt="Lilly Pad" class="absolute w-40 bottom-2 z-10" />
+					</div>
+				</div>
+			</div>
+			
+		</div>
 	</div>
 
-	<img src={hills} alt="Hills" class="w-full block ml-auto mr-auto left-0 absolute z-10 top-28"/>
-	<img src={grass} alt="Grass" class="w-full block ml-auto mr-auto bottom-[-155px] left-0 absolute z-10" />
-	<img src={side_leaves} alt="Side Leaves" class="block w-2/5 ml-auto mr-auto top-0 right-0 absolute z-30" />
-	
-	<img src={dark_waves} alt="Dark Waves" class="w-full block absolute left-0 z-10 bottom-0" />
-	<img src={light_waves} alt="Light Waves" class="w-full block absolute left-0 z-10 bottom-0" />
-	<div class="absolute bg-[#78BFCE] left-0 w-full bottom=[-700px] h-full z-20"></div>
 
-	</div>
 
 		<!--
 		<button id="apply-now">Apply Now!</button>
