dbd1f6a8626caf304e73dc93f4724e5ae444565d
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-08-07 21:02:00 -0700

    fix nav bar

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -3,61 +3,64 @@
   import instagram from '$lib/Assets/Landing/instagram.png';
   import tiktok from '$lib/Assets/Landing/tiktok.png';
   import linkedin from '$lib/Assets/Landing/linkedin.png';
-	export let navBar = [
+
+	let navBar = [
 		{ name: "Home", link: "/" },
 		{ name: "Event", link: "/event" },
 		{ name: "About", link: "/about" },
 		{ name: "FAQ", link: "/faq" }
 	];
+
+	let socialMedia = [
+		{ name: "Instagram", link: "https://www.instagram.com/fullybeyond/" , icon: instagram},
+		{ name: "Tiktok", link: "https://www.tiktok.com/fullybeyond", icon: tiktok },
+		{ name: "Linkedin", link: "https://www.linkedin.com/company/fullybeyond", icon: linkedin }
+	];
+
+	let showMenu = false;
+
+	function toggleMenu() {
+		showMenu = !showMenu;
+	}
+	
 </script>
 
 <main>
-  <ul id="nav-bar">
-    <img id="nav-logo" src={logo} alt="FullyBeyond" />
-    <li id="fullybeyond-text">FullyBeyond</li>
-    <li class="divider"></li>
+		<nav class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center bg-[#FFF2DF] rounded-3xl border-[#B75D77] border-2">
+			<div class="flex items-center justify-between">
+				<div>
+					<a href="/" class="text-2xl font-bold text-gray-800 md:text-3xl">
+						<img src={logo} alt="Fully Beyond" class="w-6" />
+					</a>
+				</div>
 
-    {#each navBar as { name, link }}
-			<li><a href={link}>{name}</a></li>
-		{/each}
-		
-    <li class="divider"></li>
-    <img src={tiktok} alt="TikTok" class="social-media" />
-    <img src={instagram} alt="Instagram" class="social-media" />
-    <img src={linkedin} alt="LinkedIn" class="social-media" />
-  </ul>
+				<!-- Mobile menu button -->
+				<button on:click={toggleMenu} class="flex md:hidden">
+					<button type="button" class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+					</button>
+				</button>
+			</div>
+
+			<!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+			<div class={showMenu ? "block" : "hidden"}> <!--If show menu is false then keep it hidden. If show menu is true then show it.-->
+				<div class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0 {showMenu
+          ? 'flex'
+          : 'hidden'}">
+					<div class="flex flex-col items-center space-y-4 md:flex-row md:space-y-0">
+						{#each navBar as item}
+						<a href={item.link} class="text-lg font-medium text-gray-800 hover:text-gray-400">{item.name}</a>
+						{/each}
+					</div>
+
+					<div class="flex items-center space-x-4 ">
+						{#each socialMedia as item}
+						<a href={item.link} target="_blank" class="text-gray-800 hover:text-gray-400">
+							<img src={item.icon} alt={item.name} class="w-6" />
+						</a>
+						{/each}
+					</div>
+				</div>
+		</nav>
 </main>
-
-<style>
-  	ul {
-		border-radius: 80px;
-		background-color: rgb(255, 242, 223);
-		border: solid 3px rgb(183, 93, 119);
-		list-style-type: none;
-		width: 65%;
-		height: 70px;
-		position: relative;
-		margin: 0 auto;
-		text-align: center;
-		padding: 10px;
-		font-size: 20px;
-	}
-
-	li {
-		display: inline;
-		margin: 25px;
-	}
-
-	#nav-logo,
-	.social-media {
-		width: 30px;
-	}
-
-	.divider {
-		border: 1px solid rgb(217, 217, 217);
-	}
-
-	#fullybeyond-text {
-		font-weight: 800;
-	}
-</style>
