e582edce6f4508ff819c1b696c870fbf78b54a78
Ola <oyinkansola_o@yahoo.com>
2024-08-18 15:29:00 -0700

    made it responsive me thinks

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -10,47 +10,52 @@
     import acmlogo from "../lib/Assets/About/acmlogo.svg"
 </script>
 
-<main class="flex flex-col items-center justify-center text-center p-2.5">
-    <img class="w-36 py-5" src="{lotus}" alt="lotus-flower"/>
+<main class="flex flex-col items-center justify-center text-center p-4 sm:p-6 md:p-8">
+    <!-- not sure if I should make the  flower smaller or bigger -->
+    <img class="w-24 sm:w-30 lg:w-36 py-5 " src="{lotus}" alt="lotus-flower"/>
 
-    <h1 class="text-7xl font-black uppercase text-[#6BD1A9] stroke-5">About US</h1>
+    <h1 class="text-5xl sm:text-6xl md:text-8xl font-black uppercase text-[#6BD1A9] stroke-5 ">About US</h1>
     
-    <div class="flex flex-wrap font-bold justify-center p-5 w-full gap-2.5">
-        <div class="bg-[#F5ECDE] w-2/5 h-4/5 rounded-3xl p-5 m-5">
-            <h2 class="text-[#959A25] text-2xl">What is FullyBeyond?</h2>
-            <p class="text-[#294105]  text-1xl py-6">
+    <div class="flex flex-wrap flex-col md:flex-row font-bold justify-center p-4 sm:p-5 md:p-8 w-full gap-4">
+        <div class="bg-[#F5ECDE] w-full md:w-2/5  h-auto rounded-3xl p-5 m-5">
+            <h2 class="text-[#959A25] text-xl md:text-2xl">What is FullyBeyond?</h2>
+            <p class="text-[#294105]  text-base sm:text-lg py-4">
                 Fully Beyond is the CSUF Designathon 2024 theme, inviting you to leapfrog conventional design thinking and explore the unexplored. 
                 Here, we'll delve deep into the vibrant world of inclusivity and diversity as the cornerstones of our design process.
             </p>
 
-            <p class="text-[#294105]">
+            <p class="text-base  sm:text-lg text-[#294105]">
                 Just like a lush rainforest teems with life, design flourishes when it embraces a variety of perspectives. 
                 We believe in empathy, equity, and celebrating differences. By going Fully Beyond, we can unlock the power of
                 design to shape a future that reflects the richness of our ever-evolving society.
             </p>
         </div>
-        <div class =  "flex justify-center items-center w-1/4 h-1/4 p-5 m-5">
+
+
+        <div class =  "flex justify-center items-center w-full  lg:w-1/4 h-auto p-5 m-5">
             <img class="acm-logo w-full h-auto"  src="{acmlogo}" alt="acm-logo">
         </div>
     </div>
 
-    <img class="w-36 py-5" src="{lotus}" alt="lotus-flower"/> 
-    <h1 class="text-6xl font-black uppercase text-[#6BD1A9] stroke-5 py-6">What Can We Look Forward To?</h1>
+
+
+    <img class="w-24 sm:w-30 lg:w-36 py-5 " src="{lotus}" alt="lotus-flower"/>
+    <h1 class="text-5xl sm:text-6xl md:text-8xl font-black uppercase text-[#6BD1A9] stroke-5 py-6">What Can We Look Forward To?</h1>
     
 
-    <div class="p-12">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mx-auto max-w-7xl">
-            <div class="bg-[#B5E8FF] border-2 border-white font-bold rounded-2xl w-full h-[410px] flex flex-col justify-center items-center">
-                <img class="w-full h-3/4 object-contain p-5" src="{fully_the_frog_fly}" alt="">
-                <p class="m-0 text-[#114674] text-2xl px-2.5 text-center"><span>Challenge and Climb with </span>fellow designers</p>
+    <div class="p-4 sm:p-6 md:p-8">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 md:gap-12 mx-auto max-w-7xl">
+            <div class="bg-[#B5E8FF] border-2 border-white font-bold rounded-2xl w-full h-[410px] flex flex-col justify-center items-center hover:scale-105  transform hover:bg-[#9DCCE4] hover:border-[#83B0CC] transition-transform duration-300">
+                <img class="w-full h-3/4 object-contain  p-4 sm:p-5" src="{fully_the_frog_fly}" alt="">
+                <p class="m-0 text-[#114674]  text-xl sm:text-2xl px-2.5 text-center"><span>Challenge and Climb with </span>fellow designers</p>
             </div>
-            <div class="bg-[#B5E8FF] border-2 font-bold border-white rounded-2xl w-full h-[410px] flex flex-col justify-center items-center">
-                <img class="w-full h-3/4 object-contain p-5" src="{fully_once_the_frog}" alt="">
-                <p class="m-0 text-[#114674] text-2xl px-2.5 text-center"><span>Clash for top prices from our </span>sponsors</p>
+            <div class="bg-[#B5E8FF] border-2 font-bold border-white rounded-2xl w-full h-[410px] flex flex-col justify-center items-center  hover:scale-105 transform hover:bg-[#9DCCE4] hover:border-[#83B0CC] transition-transform duration-300">
+                <img class="w-full h-3/4 object-contain p-4 sm:p-5" src="{fully_once_the_frog}" alt="">
+                <p class="m-0 text-[#114674]  text-xl sm:text-2xl px-2.5 text-center"><span>Clash for top prices from our </span>sponsors</p>
             </div>
-            <div class="bg-[#B5E8FF] border-2 font-bold border-white rounded-2xl w-full h-[410px] flex flex-col justify-center items-center">
-                <img class="w-full h-3/4 object-contain p-5" src="{funna}" alt="">
-                <p class="m-0 text-[#114674] text-2xl px-2.5 text-center"><span>Get industry feedbacks from</span> mentors</p>
+            <div class="bg-[#B5E8FF] border-2 font-bold border-white rounded-2xl w-full h-[410px] flex flex-col justify-center items-center  hover:scale-105 transform hover:bg-[#9DCCE4] hover:border-[#83B0CC] transition-transform duration-300">
+                <img class="w-full h-3/4 object-contain p-4 sm:p-5" src="{funna}" alt="">
+                <p class="m-0 text-[#114674]  text-xl sm:text-2xl px-2.5 text-center"><span>Get industry feedbacks from</span> mentors</p>
             </div>
         </div>
     </div>
