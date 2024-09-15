22f0e189bd2a18149ac371596fcd168a78cffca4
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-09-14 22:49:47 -0700

    Edit FAQ Duration

--- src/components/Accordion.svelte
+++ src/components/Accordion.svelte
@@ -13,17 +13,18 @@
 
 <button on:click={handleClick} class="accordion w-full h-full m-1">
     <div class="header h-full w-full flex">
-        <div class="text w-full p-4 flex items-center justify-between"
-            style="border-bottom-left-radius: {open ? '0' : '1rem'}; border-bottom-right-radius: {open ? '0' : '1rem'};">
+        <div class="text w-full p-4 flex items-center justify-between {open ? 'open' : 'closed'}">
             <slot name="head"></slot>
+            <!-- SVG icon changes depending on the 'open' state -->
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d={open ? "M19 9l-7 7-7-7" : "M5 15l7-7 7 7"} />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d={open ? "M19 9l-7 7-7-7" : "M5 15l7-7 7 7"} />
             </svg>
         </div>
     </div>
 
     {#if open}
-    <div class="details" transition:slide>
+    <div class="details" transition:slide={{ duration: 150 }}>
         <slot name="details"></slot>
     </div>
     {/if}
@@ -39,7 +40,15 @@
         border-radius: 2rem;
         /*background-color: '#FFF3E5';*/
     }
-      
+    
+    .open {
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+    }
+    .closed {
+        border-bottom-left-radius: 1rem;
+        border-bottom-right-radius: 1rem;
+    }
     
     div.header .text {
         /* flex: 1; */
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // @type {import('tailwindcss').Config} 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  purge: ['./src/**/*.svelte', './src/**/*.html'],
   theme: {
     extend: {
       fontFamily: {
