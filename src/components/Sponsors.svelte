73006529428cc76e8952946c78ccb798f5916b18
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-09-28 14:44:23 -0700

    Remove unnecessary importing

--- src/components/Sponsors.svelte
+++ src/components/Sponsors.svelte
@@ -1,8 +1,5 @@
 <svelte:head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </svelte:head>
 
 
